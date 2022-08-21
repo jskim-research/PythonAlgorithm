60e80ce70ff7c6e1e1df7d81de188b0d41ea4aac
jskim-research <wltnwkd21@gmail.com>
2022-08-21 21:18:04 +0900

    Re-solve: programmers 짝 지어 제거하기 문제 재풀이

--- programmers/level2/PairRemoval.py
+++ programmers/level2/PairRemoval.py
@@ -8,6 +8,11 @@ programmers 짝지어 제거하기 문제풀이
          데이터를 보는 방식에 순서가 있기 때문에 모든 case를 보고 작은 case 들을 병합해나가는 과정보다
          divide 하고 순차적으로 병합하는 과정이 훨씬 빠름.
 개선 방법: 문제의 가정 상황을 잘 이용해먹자.... !!!! 근데 그냥 stack (python 에선 list)만 이용해도 되는 문제임. 어떤 자료구조가 필요한지 항상 생각하자.
+
+2차 시도
+시작 시간: 2022.08.21 21:10
+끝 시간: 2022.08.21 21:17
+걸린 시간: 7분
 """
 def divide_conquer(s):
     len_s = len(s)
@@ -56,4 +61,18 @@ def simple_solution(s):
 
 print(simple_solution("abba"))
 print(simple_solution("abbc"))
+
+
+def solution2(s):
+    stack = []
+    for c in s:
+        if len(stack) == 0:
+            stack.append(c)
+        else:
+            if stack[-1] == c:
+                stack.pop()
+            else:
+                stack.append(c)
+
+    return 1 if len(stack) == 0 else 0
 
