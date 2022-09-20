d7123f40e5e296ac202651312f20be7ba171cbc3
jskim-research <wltnwkd21@gmail.com>
2022-09-21 07:38:33 +0900

    Feat: implement 2d coord rotation function

--- library.py
+++ library.py
@@ -3,6 +3,7 @@
 
 최대한 가독성, 효율성 을 생각하여 다듬을 예정
 """
+import math
 
 
 def count_digit(number: int):
@@ -15,3 +16,23 @@ def count_digit(number: int):
         0~9 => return 1, 10~99 => return 2, 100~999 => return 3, ...
     """
     return len(str(number))
+
+
+def rotate_2d_coord(x: float, y: float, theta: float):
+    """
+    rotate x, y by theta (from right to left)
+
+    Args:
+        x: x
+        y: y
+        theta: degree
+    Returns:
+        x, y rotated by theta
+    """
+    theta = math.radians(theta)
+    # (cos(a)x + sin(a)y => cos(a+theta)x + sin(a+theta)y 정리해보면 밑과 같이 나옴)
+    ret_x = math.cos(theta) * x - math.sin(theta) * y
+    ret_y = math.sin(theta) * x + math.cos(theta) * y
+    return ret_x, ret_y
+
+
