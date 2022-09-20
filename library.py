7e79aed359e4a0e126204a2bf1f245f27bb12c73
jskim-research <wltnwkd21@gmail.com>
2022-09-21 07:48:46 +0900

    Feat: implement matrix rotation by 90 degree

--- library.py
+++ library.py
@@ -4,6 +4,7 @@
 최대한 가독성, 효율성 을 생각하여 다듬을 예정
 """
 import math
+from typing import List
 
 
 def count_digit(number: int):
@@ -36,3 +37,27 @@ def rotate_2d_coord(x: float, y: float, theta: float):
     return ret_x, ret_y
 
 
+def rotate_matrix_90_degree(mat: List[List[float]]):
+    """
+    rotate matrix by 90 degree
+
+    Args:
+        mat: target matrix which has rectangular shape
+    Returns:
+        rotated matrix by 90 degree
+    """
+    size = len(mat)
+    if size <= 0 or size != len(mat[0]):
+        raise ValueError("size should be larger than 0 or "
+                         "the matrix should be rectangular which has same width and height")
+
+    ret_mat = [[0] * size for _ in range(size)]
+    for i in range(0, size):
+        for j in range(0, size):
+            ret_mat[j][size-1-i] = mat[i][j]
+    return ret_mat
+
+
+if __name__ == "__main__":
+    mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(rotate_matrix_90_degree(mat))
